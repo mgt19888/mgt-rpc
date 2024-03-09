4b46166243da7f756a30a8b6a1d49975f6f9512d
mgt19888 <1152719547@qq.com>
2024-03-09 23:35:07 +0800

    fit: 完善服务提供者示例

--- example-provider/src/main/java/com/mgt/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/mgt/example/provider/EasyProviderExample.java
@@ -1,5 +1,13 @@
 package com.mgt.example.provider;
 
+import com.mgt.example.common.service.UserService;
+import com.mgt.mgtrpc.RpcApplication;
+import com.mgt.mgtrpc.config.RegistryConfig;
+import com.mgt.mgtrpc.config.RpcConfig;
+import com.mgt.mgtrpc.model.ServiceMetaInfo;
+import com.mgt.mgtrpc.registry.LocalRegistry;
+import com.mgt.mgtrpc.registry.Registry;
+import com.mgt.mgtrpc.registry.RegistryFactory;
 import com.mgt.mgtrpc.server.HttpServer;
 import com.mgt.mgtrpc.server.VertxHttpServer;
 
@@ -9,8 +17,28 @@ import com.mgt.mgtrpc.server.VertxHttpServer;
 public class EasyProviderExample {
 
     public static void main(String[] args) {
+        // RPC 框架初始化
+        RpcApplication.init();
+
+        // 注册服务
+        String serviceName = UserService.class.getName();
+        LocalRegistry.register(serviceName, UserServiceImpl.class);
+
+        // 注册服务到注册中心
+        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+        RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
+        Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName(serviceName);
+        serviceMetaInfo.setServiceAddress(rpcConfig.getServerHost() + ":" + rpcConfig.getServerPort());
+        try {
+            registry.register(serviceMetaInfo);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         // 启动 web 服务
         HttpServer httpServer = new VertxHttpServer();
-        httpServer.doStart(8080);
+        httpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
     }
 }
--- example-provider/src/main/java/com/mgt/example/provider/UserServiceImpl.java
+++ example-provider/src/main/java/com/mgt/example/provider/UserServiceImpl.java
@@ -10,7 +10,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(User user) {
-        System.out.println("用户名" + user.getName());
+        System.out.println("用户名：" + user.getName());
         return user;
     }
 }
