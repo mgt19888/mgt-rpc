7bc513ca87beaa012267157e5a45d81f70883526
mgt19888 <1152719547@qq.com>
2024-03-09 23:34:58 +0800

    fit: 完善服务消费者示例

--- example-consumer/src/main/java/com/mgt/example/consumer/EasyComsumerExample.java
+++ example-consumer/src/main/java/com/mgt/example/consumer/EasyComsumerExample.java
@@ -2,14 +2,27 @@ package com.mgt.example.consumer;
 
 import com.mgt.example.common.model.User;
 import com.mgt.example.common.service.UserService;
+import com.mgt.mgtrpc.config.RpcConfig;
+import com.mgt.mgtrpc.proxy.ServiceProxyFactory;
+import com.mgt.mgtrpc.utils.ConfigUtils;
 
 public class EasyComsumerExample {
 
     public static void main(String[] args) {
-        UserService userService = null;
+        RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
+        System.out.println(rpc);
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
         User user = new User();
         user.setName("mgt");
-        //调用
-        userService.getUser(user);
+        // 调用
+        User newUser = userService.getUser(user);
+
+        if (newUser != null) {
+            System.out.println(newUser.getName());
+        } else {
+            System.out.println("user == null");
+        }
+        long number = userService.getNumber();
+        System.out.println(number);
     }
 }
